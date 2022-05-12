c4ee185ba381171c927ed08b9561fce2ebaa6a46
Fabian Gutiérrez Escamilla <fabian.gti@hotmail.com>
2022-05-12 13:28:41 -0500

    Initial commit

---
+++ src/main/java/com/example/springbootservice/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.example.springbootservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	private static Logger logger = LoggerFactory.getLogger(DemoController.class);
+	
+	@Autowired
+	private DockClient client;
+	
+	@GetMapping("/demon/{name}")
+	public String getHome(@PathVariable("name") String name) {
+		logger.info("Boot request {}", name);
+		String value = client.getHome(name);
+		logger.info("Boot response {}", value);
+		return value;
+	}
+
+}
---
+++ src/main/java/com/example/springbootservice/controller/DockClient.java
@@ -0,0 +1,14 @@
+package com.example.springbootservice.controller;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient("spring-boot-docker")
+public interface DockClient {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/hellgate/{name}")
+	String getHome(@PathVariable("name") String name);
+
+}
